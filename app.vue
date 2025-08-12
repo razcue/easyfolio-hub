0707729b04f4542369f6d42a97e20704e2fa8127
Rayko(razcue) <razcue@yandex.com>
2025-08-12 14:34:43 -0400

    feat: add NuxtPage for router-driven pages

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
-	<div
-		class="min-h-screen bg-[var(--color-bg-mute)] text-[var(--color-text)] transition-colors duration-500 font-sans leading-relaxed"
-	>
-		<NuxtRouteAnnouncer />
-		<Home />
-	</div>
+  <div
+      class="min-h-screen bg-[var(--color-bg-mute)] text-[var(--color-text)] transition-colors duration-500 font-sans leading-relaxed"
+  >
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
 </template>
